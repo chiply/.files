f3e82232d6abf839be459120f14dbc0171f99d21
Charlie Baker <misterchiply@gmail.com>
2022-11-13 22:47:14 -0500

    changes

--- .zetta.d/source/config/citar.el
+++ .zetta.d/source/config/citar.el
@@ -1,11 +1,32 @@
 (use-package citar
-  ;;:bind (("C-c b" . citar-insert-citation)
-         ;;:map minibuffer-local-map
-         ;;("M-b" . citar-insert-preset))
-  ;;:custom
-  ;;(citar-bibliography '("~/bib/references.bib"))
+  :config
+  (setq citar-symbols
+        `((file ,(all-the-icons-faicon "file-o"
+                                       :face 'all-the-icons-red
+                                       :v-adjust -0.1) . " ")
+          (note ,(all-the-icons-material "speaker_notes"
+                                         :face 'all-the-icons-red
+                                         :v-adjust -0.3) . " ")
+          (link ,(all-the-icons-octicon "link"
+                                        :face'all-the-icons-red
+                                        :v-adjust 0.01) . " ")))
+  (setq citar-symbol-separator "  ")
+
+  ;; NOT RECURSIVE!!! Need to specify explicitly
+  (setq
+   citar-library-paths `(
+                         ,(expand-file-name "~/.files/.lit/pdf")
+                         )
+   )
+
+
   :custom
   (citar-bibliography '("~/.files/.lit/bibliography.bib"))
   )
 
+
+(use-package citar-embark
+  :after citar embark
+  :no-require
+  :config (citar-embark-mode))
 
