b68dda535979e306d7871c4ff668a94eeaf2d57c
Charlie Holland <mister.chiply@gmail.com>
2022-11-18 13:21:59 -0500

    changes

--- .zetta.d/source/config/org.el
+++ .zetta.d/source/config/org.el
@@ -1177,6 +1177,8 @@ With a prefix ARG, remove start location."
 
 
 (use-package org-modern
+  :config
+  (setq org-modern-table nil)
   :hook (org-mode . (lambda ()
                       (org-indent-mode -1)
                       (org-modern-mode 1)))
