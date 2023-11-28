f04afd92b1303d30d9e86decd099e8355ccd7a7b
Charlie Holland <mister.chiply@gmail.com>
2023-11-27 19:52:47 -0500

    c

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,56 +5,48 @@ touch ~/.localsecrets
 xcode-select --install
 
 # install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# housekeeping
+/bin/bash \
+    -c \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew upgrade
+
 export GRAPHVIZ_DIR="$(brew --prefix graphviz)"
 
-
 # sync dot files
-brew install git
-#git clone https://github.com/chiply/.zetta.d
-
-brew intall python
+brew install git python
 python3 ~/.files/main.py
 
 # install bundle
 brew tap Homebrew/bundle
 
-
 # installs everything ~/Brewfile
 brew bundle
 # to update the brewfile and the system
-# uninstalls anything not included in the brew bundle file
+# uninstalls anything not included in the brew bundle file:
 # brew bundle --force cleanup
 # .. manual edits
-# rewrites the Brewfile
+# rewrites the Brewfile:
 # brew bundle dump
 
-# znap
+# znap: znap is a plugin manager for zsh that's simple, fast, and easy
+# to use.
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git
 source zsh-snap/install.zsh
 
-
-
-
-
-# install autocomplete
+# install autocomplete: incremental narrowing completing read
 znap source marlonrichert/zsh-autocomplete
 
-# install autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# install autosuggestions: fish-like autosuggestions for zsh
+git clone \
+    https://github.com/zsh-users/zsh-autosuggestions \
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
-
-# aws cli v2
+# AWS cli v2
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 
-
-
-# tmuxinator
+# tmuxinator: manage complex tmux sessions easily
 gem install tmuxinator
 
 # cheat.sh (not cheat)
@@ -65,13 +57,9 @@ curl -s https://cht.sh/:cht.sh | \
 # how-2
 npm install -g how-2
 
-
+# syntax highlighting in the command line
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-
-
-
 
 # user friendly emacs distros
 git clone https://github.com/plexus/chemacs.git
@@ -80,7 +68,14 @@ cd chemacs
 # note the ~/.emacs-profiles.el file which refers to these directories
 
 ## spacemacs
-# git clone https://github.com/syl20bnr/spacemacs ~/.spacemacs.d
+# clone or update spacemacs
+#if [ -d "~/.spacemacs.d" ]; then
+    #cd ~/.spacemacs.d
+    #git pull
+#else
+    #git clone https://github.com/syl20bnr/spacemacs ~/.spacemacs.d
+#fi
+    
 
 ## doom
 # git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.doom.d && \
