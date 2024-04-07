68775a7fbbc1218f1d3995778b684d4fb09d972c
Charlie Holland <mister.chiply@gmail.com>
2024-04-07 10:43:16 -0400

    c

--- .zetta.d/source/config/detached.el
+++ .zetta.d/source/config/detached.el
@@ -1,12 +1,6 @@
-(use-package alert
-  :config
-  (setq alert-default-style 'osx-notifier))
-
-
-
-;; (use-package detached
-;;   :init
-;;   (detached-init)
-;;   :custom ((detached-show-output-on-attach t)
-;;           (detached-terminal-data-command system-type)
-;;            (detached-notification-function #'detached-extra-alert-notification)))
+(use-package alert :config (setq alert-default-style 'osx-notifier))
+(use-package detached
+  :init (detached-init)
+  :custom ((detached-show-output-on-attach t)
+           (detached-terminal-data-command system-type)
+           (detached-notification-function #'detached-extra-alert-notification)))
