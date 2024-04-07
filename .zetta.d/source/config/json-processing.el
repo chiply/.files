5312286d8c172a869c81fbf95439f6bff2db6e4e
Charlie Holland <mister.chiply@gmail.com>
2024-04-07 15:41:37 -0400

    c

--- .zetta.d/source/config/json-processing.el
+++ .zetta.d/source/config/json-processing.el
@@ -10,38 +10,30 @@
 
 ;; does comint work in compilation mode?
 
-
-
-
+(templatel-render-string "{{ i }} {{ foo }}" '(("i" . "j") ("foo" . "bar")))
 
 (defun prepare-for-completing-read (data)
-  (-map (lambda (x)
-          `(,(mapconcat
-              (lambda (x) (if (numberp x) (number-to-string x) x))
-              (ht-values x) " ")
-            . ,x))
-        data))
+  (-map
+   (lambda (x)
+     `(,(mapconcat
+         (lambda (x)
+           (if (numberp x) (number-to-string x) x))
+         (ht-values x)
+         " ")
+       . ,x))
+   data))
 
 ;; TODO define a function that pulls the value out of alist
 (defun completing-read-value (prompt list)
-  (let* (
-         ;; TODO attempting to stop orderless from re-ordering
-         ;;(completion-category-overrides nil)
-         ;;(completion-category-defaults nil)
-         ;;(completion-styles '(basic))
-         (selected-run-key (let ((vertico-sort-function nil)) (completing-read prompt list)))
-         (selected-run (alist-get selected-run-key list nil nil 'string=)))
-    selected-run))
+  (let* ((selected-run-key (let ((vertico-sort-function nil)) (completing-read prompt list))))
+    (alist-get selected-run-key list nil nil 'string=)))
 
 (defun z-gh-pick-run (dir cmd)
   (interactive)
   (let* (;; TODO have projectile prompt for a project or use the default
          (default-directory dir)
-         ;; run the command
          (json (shell-command-to-string cmd))
          (json-ht (json-parse-string json))
-         ;; LEFT OFF TODO SORT -- can't get this to work
-         ;; (json-ht (-sort (lambda (a b) (< (ht-get a "updatedAt") (ht-get b "updatedAt"))) json-ht))
          (runs (prepare-for-completing-read json-ht)))
     (completing-read-value "" runs)))
 
@@ -52,7 +44,6 @@
                                   (if nosleep "" "sleep 5 && ")
                                   "gh run list --json conclusion,databaseId,displayTitle,event,headBranch,name,startedAt,status,updatedAt,workflowDatabaseId --jq ' [.[] | select(.updatedAt > (now - ( 0.1 * 86400))) ]'")))
          (id (ht-get run "databaseId"))
-         ;; todo add osascript
          (cmd (format
                "gh run watch -i 30 %s; osascript -e 'display notification \"%s %s\" with title \"%s 🐙 %s\" sound name \"Frog\"'"
                id
@@ -60,29 +51,17 @@
                (concat "on " (ht-get run "headBranch"))
                (projectile-project-name dir)
                (ht-get run "name")))
-         )
-    ;; vterm
-    ;;(let* ((bufnm (format "*GHA: %s 🐙 %s*" (projectile-project-name dir) (ht-get run "name"))))
-    ;;(when (get-buffer bufnm)
-    ;;(kill-buffer bufnm))
-    ;;(let* ((vterm-buffer (save-window-excursion (vterm bufnm)))
-    ;;(vterm-process (get-buffer-process vterm-buffer)))
-    ;;(process-send-string vterm-process (concat cmd "\n"))
-    ;;bufnm
-    ;;(zmc-display-output-buffer bufnm 'top 1)))
-    ;; compile
-    (let* ((bufnm (format "*GHA: %s 🐙 %s*" (projectile-project-name default-directory) (ht-get run "name")))
-           (compilation-buffer-name-function
-            `(lambda (_) ,bufnm)))
-      (save-window-excursion (detached-compile cmd))
-      (zmc-display-output-buffer bufnm 'top 1))))
+         (bufnm (format "*GHA: %s 🐙 %s*" (projectile-project-name default-directory) (ht-get run "name")))
+         (compilation-buffer-name-function `(lambda (_) ,bufnm)))
+    (save-window-excursion (detached-compile cmd))
+    (zmc-display-output-buffer bufnm 'top 1)))
 
 (defun z-gh-run-view-log (dir)
   (let* ((default-directory dir)
          (run (z-gh-pick-run dir "gh run list --json conclusion,databaseId,displayTitle,event,headBranch,name,startedAt,status,updatedAt,workflowDatabaseId --jq ' [.[]| select(.updatedAt > (now-( 30 * 86400)))]'"))
          (id (ht-get run "databaseId"))
          (cmd (format "gh run view %s --log" id)))
-    (shell-command cmd)))
+    (shell-command cmd (format "*GHA: %s 🐙 %s*" (projectile-project-name default-directory) (ht-get run "name")))))
 
 
 ;; UI
