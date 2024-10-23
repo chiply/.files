e6e06a74449c57a28645f8e63cf3520e0397dbe1
Charlie Holland <mister.chiply@gmail.com>
2024-10-23 13:23:54 -0400

    c

--- .zetta.d/source/config/gnus.el
+++ .zetta.d/source/config/gnus.el
@@ -5,4 +5,4 @@
   (setq gnus-select-method '(nnnil ""))
   (setq gnus-secondary-select-methods '((nntp "news.gmane.io")))
   (setq gnus-check-new-newsgroups 'ask-server)
-  (setq gnus-startup-file (expand-file-name ".newsrc" user-emacs-directory)))
+  )
