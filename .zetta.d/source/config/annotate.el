5fb2eab4049be91dc7f07572b93e0e7b938a7e47
Charlie Holland <mister.chiply@gmail.com>
2025-01-21 00:28:26 -0500

    fix: c

--- .zetta.d/source/config/annotate.el
+++ .zetta.d/source/config/annotate.el
@@ -3,9 +3,6 @@
 
   ;; test annotation
 
-  ;; NOTE when adding text, need to re-save annotations using
-  ;; `annotate-save-annotations`, otherwise adding text corrupts the
-  ;; data
 
   ;; NOTE annotations could be on possibly private data, but would be
   ;; ideal to sync annotations for things like `.files` (via github)
@@ -15,11 +12,16 @@
   ;; annotations flie.  Need to explicitly set the .dir-locals on each
   ;; machine and to make annotations havea .org suffix for annotations
   ;; to get synced)
-  (setq annotate-file "~/annotations")
+  ;; (setq annotate-file "~/annotations")
   
+  ;; NOTE This solves the problem of syncing public vs private... keep
+  ;; in mind the alternative .dir-locals + annotations approach was
+  ;; not working!
+  (setq annotate-file-buffer-local t)
+  (setq annotate-buffer-local-database-extension "annotations.org")
   ;; otherwise you get alignment issues
-  ;; NOTE doesn't seem to use all colors
   (setq annotate-annotation-column 0)
+  ;; NOTE doesn't seem to use all colors
   (setq
    annotate-highlight-faces
    '((:background "#EEF192")
@@ -45,20 +47,33 @@
      (:background "#F1F192" :underline t)
      (:background "#F192F1" :underline t)
      (:background "#92F1F1" :underline t)
-     (:background "#F1F1F1" :underline t)
-     )
-   )
+     (:background "#F1F1F1" :underline t)))
 
   ;; NOTE may be missing bindings for meow
+
+  ;; NOTE when adding text, need to re-save annotations using
+  ;; `annotate-save-annotations`, otherwise adding text corrupts the
+  ;; data
   :general
   (
    :keymaps '(markdown-mode-map org-mode-map prog-mode-map)
    :states '(normal visual insert)
-   "C-s" (lambda () (interactive) (annotate-save-annotations) (save-buffer))
+   "C-s" (lambda () (interactive)
+           (when (bound-and-true-p annotate-mode)
+             (annotate-save-annotations))
+           (save-buffer))
+   )
+  (
+   :keymaps 'menu-window-keymap
+   "a" 'annotate-mode
    )
 
-  ;; NOTE add embark general binding for annotate
+  ;; TODO add embark general binding for annotate
 
-  :hook ((prog-mode markdown-mode org-mode) . annotate-mode)
-  
-  )
+  ;; TODO add autosave hook to ensure annotations are being saved if
+  ;; autosaved?
+
+  ;; TODO something to ensure annotations are saved whenever an
+  ;; annotation is added? or will i simply have to remember this
+
+  :hook ((markdown-mode org-mode prog-mode) . annotate-mode))
