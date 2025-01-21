01c55109645a82ea45952e89d71d325dd97fd7a8
Charlie Holland <mister.chiply@gmail.com>
2025-01-20 23:28:38 -0500

    fix: c

--- files/.emacs-profiles.el
+++ files/.emacs-profiles.el
@@ -1,4 +1,6 @@
-(("zetta" . ((user-emacs-directory . "~/.files/.zetta.d")))
+(
+ ("default" . ((user-emacs-directory . "~/.files/.zetta.d")))
+ ("zetta" . ((user-emacs-directory . "~/.files/.zetta.d")))
  ("spacemacs" . ((user-emacs-directory . "~/.spacemacs.d")))
  ("doom" . ((user-emacs-directory . "~/.doom.d")))
  ("prelude" . ((user-emacs-directory . "~/.prelude.d")))
