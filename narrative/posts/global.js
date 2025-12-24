4233f5213791a5d70ed34a111bd8bd96302b5a5c
Charles Baker <charlesbaker@Charless-MacBook-Pro.local>
2025-12-24 08:30:40 -0500

    fix: update from pc

--- narrative/posts/global.js
+++
@@ -1,7 +0,0 @@
-// Keys
-document.addEventListener("keydown", function (event) {
-    // Check for 'c' key (can modify as needed)
-    if (event.key === "c" && !event.ctrlKey && !event.metaKey) {
-        console.log("c key pressed");
-    }
-});
